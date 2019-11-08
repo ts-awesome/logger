7721c43260a8dc15a3044c0df234b7399eeb6cf9
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2019-11-08 12:06:15 +0200

    improved interfaces and documentation

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './interfaces';
 export * from './symbols'
 export * from './logger';
 export * from './utils';
+
+import Symbols from './symbols';
+export default Symbols;
--- src/symbols.ts
+++ src/symbols.ts
@@ -3,9 +3,11 @@ export const LoggerFactorySymbol = Symbol.for('LoggerFactory');
 export const LoggerDriverSymbol = Symbol.for('LoggerDriver');
 export const ErrorReporterSymbol = Symbol.for('ErrorReporter');
 
-export default {
+export const Symbols = {
   Config: ConfigSymbol,
   LoggerFactory: LoggerFactorySymbol,
   LoggerDriver: LoggerDriverSymbol,
   ErrorReporter: ErrorReporterSymbol,
-}
+};
+
+export default Symbols;
